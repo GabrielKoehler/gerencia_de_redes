c546ab70c40474db77588422d6328df232c440cb
GabrielKoehler <gabriel_koehler@hotmail.com>
2020-09-01 20:36:32 -0300

    Commit inicial, criacao de um Makefile

---
+++ atividade_1/message.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "message.h"    
+using namespace std;
+
+void message::printMessage(){
+    cout << "Gabriel de Oliveira Koehler.\nGerência de Redes.\nUbuntu 20.04.";
+}
